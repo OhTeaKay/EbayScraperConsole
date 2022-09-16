2627eb63bb3cfeb3146f4f835075ff58183c4500
OskarOhTeaKay <kozluko@L2396>
2022-09-16 23:14:23 +0200

    a

--- EbayScraperConsole/Program.cs
+++ EbayScraperConsole/Program.cs
@@ -16,7 +16,7 @@ namespace EbayScraperConsole
             GetHtmlAsync();
             Console.ReadLine();
         }
-
+        // as
         private static async void GetHtmlAsync()
         {
             var url = "https://www.ebay.pl/sch/i.html?_nkw=Playstation+5&_in_kw=1&_ex_kw=&_sacat=0&LH_Complete=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=10&_fpos=&LH_SALE_CURRENCY=0&_sop=12&_dmd=1&_ipg=200&_fosrp=1";
